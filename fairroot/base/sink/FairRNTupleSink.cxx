1e50248869732fc0fc4de1b8156985100d8d2449
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2025-04-02 11:24:52 +0200

    feat(rntuple): Add FairRNTupleSink and example macros
    Added FairRNTupleSink class to enable storing in RNTuple.
    This commit does not compile the class.
    Added macros to the propagator example
    that allows running the example with TTree or RTNuple sink.

---
+++ fairroot/base/sink/FairRNTupleSink.cxx
@@ -0,0 +1,130 @@
+/********************************************************************************
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+// -----------------------------------------------------------------------------
+// -----                                                                   -----
+// -----                             FairRNTupleSink            m          -----
+// -----                    Created 20.03.2025 by R. Karabowicz            -----
+// -----                                                                   -----
+// -----------------------------------------------------------------------------
+
+#include "FairRNTupleSink.h"
+
+#include "FairEventHeader.h"
+#include "FairMonitor.h"       // to store histograms at the end
+#include "FairRootManager.h"   // to GetTreeName()
+
+R__LOAD_LIBRARY(ROOTNTuple)
+
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <ROOT/RNTupleWriter.hxx>
+#include <TBranch.h>
+#include <TClass.h>
+#include <TCollection.h>
+#include <TFolder.h>
+#include <TGeoManager.h>   // for TGeoManager, gGeoManager
+#include <TIterator.h>
+#include <TNamed.h>
+#include <TObjArray.h>
+#include <TObject.h>
+#include <TROOT.h>
+#include <TString.h>
+#include <TTree.h>
+#include <cstdlib>    // free
+#include <cxxabi.h>   // __cxa_demangle
+#include <fairlogger/Logger.h>
+#include <string>
+
+using REntry = ROOT::Experimental::REntry;
+using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
+using RNTupleModel = ROOT::Experimental::RNTupleModel;
+
+FairRNTupleSink::FairRNTupleSink(const TString* RootFileName, const char* Title)
+    : FairSink()
+    , fOutputTitle(Title)
+    , fFileName(RootFileName->Data())
+{
+    fModel = RNTupleModel::Create();
+}
+
+FairRNTupleSink::FairRNTupleSink(const TString RootFileName, const char* Title)
+    : FairSink()
+    , fOutputTitle(Title)
+    , fFileName(RootFileName.Data())
+{
+    fModel = RNTupleModel::Create();
+}
+
+Bool_t FairRNTupleSink::InitSink()
+{
+    fIsInitialized = kTRUE;
+    return kTRUE;
+}
+
+void FairRNTupleSink::Close() {}
+
+void FairRNTupleSink::Reset() {}
+
+void FairRNTupleSink::FillEventHeader(FairEventHeader* /* feh */) {}
+
+void FairRNTupleSink::WriteFolder()
+{
+    CreatePersistentBranchesAny();
+}
+
+bool FairRNTupleSink::CreatePersistentBranchesAny()
+{
+    fWriter = RNTupleWriter::Recreate(std::move(fModel), "fairdata", fFileName);
+    fEntry = fWriter->GetModel().CreateBareEntry();
+    for (auto tokenAddress : fTokenAddress) {
+        fEntry->BindRawPtr(tokenAddress.first, tokenAddress.second);
+    }
+    fPersistentBranchesDone = true;
+    return true;
+}
+
+void FairRNTupleSink::WriteObject(TObject* f, const char* name, Int_t option)
+{
+    //    fRootFile->cd();
+    //    f->Write(name, option);
+}
+
+void FairRNTupleSink::WriteGeometry()
+{
+    //    fRootFile->WriteTObject(gGeoManager);
+}
+
+void FairRNTupleSink::Fill()
+{
+    RNTupleFillStatus status;
+    fWriter->FillNoFlush(*fEntry, status);
+    if (status.ShouldFlushCluster()) {
+        // If we are asked to flush, first try to do as much work as possible outside of the critical section:
+        // FlushColumns() will flush column data and trigger compression, but not actually write to storage.
+        // (A framework may of course also decide to flush more often.)
+        fWriter->FlushColumns();
+
+        {
+            // FlushCluster() will flush data to the underlying TFile, so it requires synchronization.
+            fWriter->FlushCluster();
+        }
+    }
+}
+
+Int_t FairRNTupleSink::Write(const char*, Int_t, Int_t)
+{
+    return 0;
+}
+//_____________________________________________________________________________
+
+//_____________________________________________________________________________
+FairSink* FairRNTupleSink::CloneSink()
+{
+    return nullptr;
+}
+//_____________________________________________________________________________
---
+++ fairroot/base/sink/FairRNTupleSink.h
@@ -0,0 +1,111 @@
+/********************************************************************************
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+// -----------------------------------------------------------------------------
+// -----                                                                   -----
+// -----                             FairRNTupleSink                       -----
+// -----                    Created 20.03.2025 by R. Karabowicz            -----
+// -----                                                                   -----
+// -----------------------------------------------------------------------------
+
+#ifndef __FAIRROOT__FairRNTupleSink__
+#define __FAIRROOT__FairRNTupleSink__
+
+#include "FairSink.h"
+
+R__LOAD_LIBRARY(ROOTNTuple)
+
+#include <ROOT/REntry.hxx>
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleFillStatus.hxx>
+#include <ROOT/RNTupleModel.hxx>
+#include <ROOT/RNTupleWriter.hxx>
+#include <Rtypes.h>
+#include <TBranch.h>
+#include <TFile.h>
+#include <TString.h>
+#include <fairlogger/Logger.h>
+#include <memory>
+#include <typeinfo>
+
+using REntry = ROOT::Experimental::REntry;
+using ROOT::Experimental::RNTupleFillStatus;
+using RNTupleModel = ROOT::Experimental::RNTupleModel;
+using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
+
+class FairEventHeader;
+class FairFileHeader;
+
+class FairRNTupleSink : public FairSink
+{
+  public:
+    FairRNTupleSink(const TString* RNTupleName, const char* Title = "OutputRNTuple");
+    FairRNTupleSink(const TString RNTupleName, const char* Title = "OutputRNTuple");
+    FairRNTupleSink(const FairRNTupleSink&) = delete;
+    FairRNTupleSink operator=(const FairRNTupleSink&) = delete;
+    ~FairRNTupleSink() override = default;
+
+    Bool_t InitSink() override;
+    void Close() override;
+    void Reset() override;
+
+    Sink_Type GetSinkType() override { return kRNTUPLESINK; }
+
+    virtual void FillEventHeader(FairEventHeader* feh);
+
+    void SetOutTree(TTree* fTree) override {}
+
+    void Fill() override;
+
+    Int_t Write(const char* name = nullptr, Int_t option = 0, Int_t bufsize = 0) override;
+
+    void RegisterImpl(const char*, const char*, void*) override{};
+    void RegisterAny(const char* brname, const std::type_info& oi, const std::type_info& pi, void* obj) override{};
+
+    template<typename T>
+    void RegisterRNTuple(const char* name, T*& obj);
+
+    void WriteFolder() override;
+    bool CreatePersistentBranchesAny() override;
+
+    void WriteObject(TObject* f, const char*, Int_t option = 0) override;
+    void WriteGeometry() override;
+
+    FairSink* CloneSink() override;
+
+  private:
+    /** Title of input sink, could be input, background or signal*/
+    std::string fOutputTitle;
+    /** ROOT file */
+    std::string fFileName;
+    /** Output Tree  */
+    /** Initialization flag, true if initialized */
+    Bool_t fIsInitialized{kFALSE};
+
+    std::unique_ptr<RNTupleModel> fModel;
+    std::unique_ptr<RNTupleWriter> fWriter;
+    std::unique_ptr<REntry> fEntry;
+
+    /**File Header*/
+    FairFileHeader* fFileHeader{nullptr};   //!
+
+    bool fPersistentBranchesDone{false};   //!
+
+    std::vector<std::pair<REntry::RFieldToken, void*>> fTokenAddress;
+
+    ClassDefOverride(FairRNTupleSink, 1);
+};
+
+template<typename T>
+void FairRNTupleSink::RegisterRNTuple(const char* brname, T*& obj)
+{
+    LOG(info) << "RegisterRNTuple(\"" << brname << "\")";
+    fModel->MakeField<T>(brname);
+    fTokenAddress.push_back(std::make_pair(fModel->GetToken(brname), obj));
+}
+
+#endif /* defined(__FAIRROOT__FairRNTupleSink__) */
